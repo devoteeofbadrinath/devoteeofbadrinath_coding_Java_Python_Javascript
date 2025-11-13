7489ab6655ed19248026bccb28efbe68dc2ee8b9
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-11-13 17:56:39 +0530

    Message

---
+++ Day3/Java/BubbleSort.java
@@ -0,0 +1,15 @@
+package Day3.Java;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int inputArray[] = new int[25];
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = (int) (Math.random() * 100);
+        }
+        for (int i : inputArray) {
+            System.out.println(i);
+        }
+    }
+
+}
