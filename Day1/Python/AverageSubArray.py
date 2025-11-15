52e8ed48d28fb2610242858eea2cac398ad72154
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-11-15 11:32:38 +0530

    Message

---
+++ Day1/Python/AverageSubArray.py
@@ -0,0 +1,8 @@
+class AverageSubArray:
+    def averageSubArray(self, input):
+        for i in range(0,len(input)):
+            print(input[i])
+
+
+averageSubArray = AverageSubArray()
+averageSubArray.averageSubArray([1,2,3,4,5])
