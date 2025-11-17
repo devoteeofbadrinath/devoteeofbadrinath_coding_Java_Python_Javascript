4882136bcee45286909799ada311b4fbaf2bc980
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-11-17 14:37:04 +0530

    Message

--- Day1/Python/AverageSubArray.py
+++ Day1/Python/AverageSubArray.py
@@ -1,11 +1,15 @@
 class AverageSubArray:
     def averageSubArray(self, input, l):
-        for i in range(0,len(input)-l+1):
-            sum = 0
-            for j in range(0,l):
-                sum  = sum + input[i+j]
+        sum = 0
+        for i in range(0,len(input)):
+            if i<l:
+                sum = sum + input[i]
+                continue
+            else:
+                #max = sum
+                sum = sum + input[i] - input[i-l]
             print(sum)
 
 
 averageSubArray = AverageSubArray()
-averageSubArray.averageSubArray([-1], 1)
+averageSubArray.averageSubArray([-1,-2,-3,-4], 1)
