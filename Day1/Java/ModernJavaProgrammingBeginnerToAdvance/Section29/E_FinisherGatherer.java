15b8328db34228ac3f2dfabfc704d68057023395
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-03-15 15:04:18 +0530

    Message

---
+++ Day1/Java/ModernJavaProgrammingBeginnerToAdvance/Section29/E_FinisherGatherer.java
@@ -0,0 +1,28 @@
+package Day1.Java.ModernJavaProgrammingBeginnerToAdvance.Section29;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Gatherer;
+import java.util.stream.Stream;
+
+public class E_FinisherGatherer {
+    public static void main(String[] args) {
+        System.out.println("Narayan");
+        Gatherer<Integer, List<Integer>, List<Integer>> batchGatherer =
+                Gatherer.ofSequential(ArrayList::new,
+                        (buffer, item, downstream) -> {
+                    buffer.add(item);
+                    if (buffer.size() == 3){
+                        downstream.push(new ArrayList<>(buffer));
+                        buffer.clear();
+                    }
+                    return true;
+                        }, (buffer, downstream) -> {
+                    if (!buffer.isEmpty()) {
+                        downstream.push(new ArrayList<>(buffer));
+                    }
+                        });
+        var result = Stream.of(1, 2, 3, 4, 5, 6, 7).gather(batchGatherer).toList();
+        System.out.println(result);
+    }
+}
---
+++ out/production/devoteeofbadrinath_coding_Java_Python_Javascript/README.md
@@ -0,0 +1,2 @@
+# devoteeofbadrinath_coding_Java_Python_Javascript
+It contains the prgrames to understand the basic of all these 3 programming languages.
