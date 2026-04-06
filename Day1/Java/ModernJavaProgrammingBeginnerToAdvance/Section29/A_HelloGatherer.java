19c76d6b4b37cbc94efe6f2b5dd214bd8e281a8c
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-04-06 14:15:50 +0530

    Message

--- Day1/Java/ModernJavaProgrammingBeginnerToAdvance/Section29/A_HelloGatherer.java
+++ Day1/Java/ModernJavaProgrammingBeginnerToAdvance/Section29/A_HelloGatherer.java
@@ -7,7 +7,7 @@ public class A_HelloGatherer {
 
     public static void main(String[] args) {
         var strings = List.of("one","two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
-        Gatherer<String,?, String> gatherer = () -> (state, element, downstream) -> true;
+        Gatherer<String,?, String> gatherer = () -> (state, element, downstream) -> downstream.push(element);
         var result = strings.stream().gather(gatherer).toList();
         System.out.println(result);
     }
