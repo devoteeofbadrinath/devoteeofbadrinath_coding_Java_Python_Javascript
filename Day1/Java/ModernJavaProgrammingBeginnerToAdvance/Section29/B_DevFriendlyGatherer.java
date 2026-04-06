45cf81af2dff0a7c25ca533fc1143e72e6ba5b34
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-04-06 14:28:53 +0530

    Message

--- Day1/Java/ModernJavaProgrammingBeginnerToAdvance/Section29/B_DevFriendlyGatherer.java
+++ Day1/Java/ModernJavaProgrammingBeginnerToAdvance/Section29/B_DevFriendlyGatherer.java
@@ -3,13 +3,16 @@ package Day1.Java.ModernJavaProgrammingBeginnerToAdvance.Section29;
 import java.util.List;
 import java.util.stream.Gatherer;
 
+import static java.util.stream.Collectors.toList;
+
 public class B_DevFriendlyGatherer {
 
     public static void main(String[] args) {
-        var strings = List.of("one","two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+        var strings = List.of("One","Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten");
         Gatherer<String,?, String> gatherer = Gatherer.of(
                 (state, element, downstream) -> downstream.push(element.toUpperCase()));
-        var result = strings.stream().gather(gatherer).toList();
+        var result = strings.stream().gather(Gatherer.of(
+                (state, element, downstream) -> downstream.push(element.toLowerCase()))).toList();
         System.out.println(result);
     }
 }
