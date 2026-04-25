c980954d0465f4fe13fad32492a662dfc90ede28
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-04-25 20:30:54 +0530

    Message

---
+++ Day1/Java/StreamsAPI/Section26/StreamCreationDemo.java
@@ -0,0 +1,10 @@
+package Day1.Java.StreamsAPI.Section26;
+
+import java.util.ArrayList;
+
+public class StreamCreationDemo {
+    public static void main(String[] args) {
+        List<String> departmentList = new ArrayList<>();
+
+    }
+}
