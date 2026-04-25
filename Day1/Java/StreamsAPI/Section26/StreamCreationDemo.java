2cb8f4dab7f3a604ff9e788ff0777a0b536ded25
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-04-25 22:53:05 +0530

    Message

--- Day1/Java/StreamsAPI/Section26/StreamCreationDemo.java
+++ Day1/Java/StreamsAPI/Section26/StreamCreationDemo.java
@@ -1,10 +1,19 @@
 package Day1.Java.StreamsAPI.Section26;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamCreationDemo {
     public static void main(String[] args) {
         List<String> departmentList = new ArrayList<>();
+        departmentList.add("Supply");
+        departmentList.add("HR");
+        departmentList.add("Sales");
+        departmentList.add("Marketing");
+
+        Stream<String> depStream = departmentList.stream();
+        depStream.forEach(department -> System.out.println(department));
 
     }
 }
