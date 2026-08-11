40fbbc81587bf7ec965e8b6adbb9220819335a8c
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-08-11 22:06:38 +0530

    Message

--- Day1/Java/DynamicProgramming/FibonacciSeries/FibonacciSeriesBottomUpApproach.java
+++ Day1/Java/DynamicProgramming/FibonacciSeries/FibonacciSeriesBottomUpApproach.java
@@ -5,17 +5,17 @@ public class FibonacciSeriesBottomUpApproach {
     public static void main(String[] args) {
         FibonacciSeriesBottomUpApproach fibonacciSeriesBottomUpApproach = new FibonacciSeriesBottomUpApproach();
         System.out.println("Shreeman Narayan Narayan Hari Hari");
-        System.out.println(fibonacciSeriesBottomUpApproach.fibonacciBottomUpApproach(50));
+        System.out.println(fibonacciSeriesBottomUpApproach.fibonacciBottomUpApproach(2));
     }
 
     int fibonacciBottomUpApproach(int n) {
-        if(n<=1)
+        if (n <= 1)
             return n;
-        int fib[] = new int[n+1];
+        int fib[] = new int[n + 1];
         fib[0] = 0;
         fib[1] = 1;
-        for(int i = 2; i<=n; i++){
-            fib[i] = fib[i-1] + fib[i-2];
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
         }
         return fib[n];
     }
