f7583a15a3a56885bc0543c4e0213e1083ff44a3
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-09-02 16:52:59 +0530

    Message

--- Day1/Java/DynamicProgramming/LCS/LCS.java
+++ Day1/Java/DynamicProgramming/LCS/LCS.java
@@ -4,10 +4,18 @@ public class LCS {
 
     public static void main(String[] args) {
         LCS lcs = new LCS();
+        System.out.println(lcs.lcs(0, 0, "ABCDEFGHIJKL", "DEFGHIJKL", 0));
     }
 
-    int lcs(String s1, String s2, int subSequenceLength) {
-        return 0;
+    int lcs(int i, int j, String s1, String s2, int subSequenceLength) {
+        if (i == s1.length() || j == s2.length()) {
+            return subSequenceLength;
+        } else if (s1.charAt(i) == s2.charAt(j)) {
+            return lcs(i + 1, j + 1, s1, s2, subSequenceLength + 1);
+        } else {
+            return Math.max(lcs(i + 1, j, s1, s2, subSequenceLength),
+                    lcs(i, j + 1, s1, s2, subSequenceLength));
+        }
     }
 
 }
