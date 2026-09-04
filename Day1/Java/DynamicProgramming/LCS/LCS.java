2a8cd65ea997f7d238634d29b839fdae96dbc4b3
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-09-04 12:43:40 +0530

    Message

--- Day1/Java/DynamicProgramming/LCS/LCS.java
+++ Day1/Java/DynamicProgramming/LCS/LCS.java
@@ -1,13 +1,16 @@
 package Day1.Java.DynamicProgramming.LCS;
 
 public class LCS {
+    int functionCallCount = 0;
 
     public static void main(String[] args) {
         LCS lcs = new LCS();
-        System.out.println(lcs.lcs(0, 0, "ABCDEFGHIJKL", "DEFGHIJKL", 0));
+        System.out.println(lcs.lcs(0, 0, "AB", "CD", 0));
+        System.out.println(lcs.functionCallCount);
     }
 
     int lcs(int i, int j, String s1, String s2, int subSequenceLength) {
+        functionCallCount++;
         if (i == s1.length() || j == s2.length()) {
             return subSequenceLength;
         } else if (s1.charAt(i) == s2.charAt(j)) {
